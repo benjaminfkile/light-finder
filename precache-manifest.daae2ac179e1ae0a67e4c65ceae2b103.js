f502495ab65a297b95e9e9cbe376832d42146b2c
ben <benjaminfkile@gmail.com>
2020-01-13 01:53:56 -0700

    Updates

---
+++ precache-manifest.daae2ac179e1ae0a67e4c65ceae2b103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f268d0ec2d7ec095a7ee03038f94f37d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8abab7bc37f5e41ec54",
+    "url": "/static/css/main.a4395dcb.chunk.css"
+  },
+  {
+    "revision": "ad183172ae0dd5f36488",
+    "url": "/static/js/2.858cc572.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.858cc572.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8abab7bc37f5e41ec54",
+    "url": "/static/js/main.2fcc2117.chunk.js"
+  },
+  {
+    "revision": "7976c00b5600443b4dc6",
+    "url": "/static/js/runtime-main.5903c785.js"
+  }
+]);
